dda0761255ed812a7b0000771a7e67bbf5783046
coltdeus <danielfernandopp@ufps.edu.co>
2021-08-04 06:21:16 -0500

    service registry con eureka

--- registry-service/src/main/java/com/pragma/customer/registry/RegistryServiceApplication.java
+++ registry-service/src/main/java/com/pragma/customer/registry/RegistryServiceApplication.java
@@ -2,7 +2,9 @@ package com.pragma.customer.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class RegistryServiceApplication {
 
